c24d1cddb3904fc6a16b2c6445af704a45aa6d07
Isedin Karamehic <isedin_karamehic@teilnehmer.cdemy.de>
2024-10-10 12:32:03 +0200

    favorite and profile UI have been done

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment_app_with_laravel_backend/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,12 +9,155 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final Map<String, dynamic>? doctor = {};
+  Map<String, dynamic>? user = {};
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Favorite Page'),
-      ),
+    return Column(
+      children: [
+        Expanded(
+          flex: 4,
+          child: Container(
+            width: double.infinity,
+            color: Config.primaryColor,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 110,
+                ),
+                CircleAvatar(
+                  radius: 65.0,
+                  backgroundImage: NetworkImage("http://127.0.0.1:8000${doctor!['doctor_profile']}"),
+                  backgroundColor: Colors.white,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Dr. ${doctor?['doctor_name'] ?? 'Unknown Doctor'}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  '39 years old',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            color: Colors.grey[200],
+            child: Center(
+              child: Card(
+                margin: const EdgeInsets.fromLTRB(0, 45, 0, 0),
+                child: Container(
+                  width: 300,
+                  height: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Personal Information',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.grey[300],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: Colors.blueAccent[400],
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Profile',
+                                style: TextStyle(
+                                  color: Config.primaryColor,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Config.spaceSmall,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.history,
+                              color: Colors.blueAccent[400],
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'History',
+                                style: TextStyle(
+                                  color: Config.primaryColor,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Config.spaceSmall,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.login_outlined,
+                              color: Colors.blueAccent[400],
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Logout',
+                                style: TextStyle(
+                                  color: Config.primaryColor,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
